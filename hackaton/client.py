a43463928edcbf9ea90d9653bbfa7b312e648778
אורי בן-ארצי <oribena@post.bgu.ac.il>
2020-12-30 16:37:35 +0200

    30.12

--- hackaton/client.py
+++ hackaton/client.py
@@ -2,6 +2,8 @@ import socket
 import time
 import struct
 import msvcrt
+import string
+import random
 import _thread as thread
 
 bufferSize = 1024
@@ -13,7 +15,7 @@ UDPClientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  # allow b
 UDPClientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 UDPClientSocket.bind(("", 13117))
 
-print("Client started' listening for offer requests")
+print("Client started, listening for offer requests...")
 t_end = time.time() + 10
 while time.time() < t_end:
     try:
@@ -21,25 +23,31 @@ while time.time() < t_end:
         msg = struct.unpack('<3Q', msg1[0])
         msg += msg1[1]
         if msg[0] == 4276993775 and msg[1] == 2:
-            print("Great success!")
+            print("ג€Received offer from " + msg[3] + ", attempting to connect...")
             tcp_port = msg[2]
-            print(tcp_port)
             try:
-                ClientSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                letters = string.ascii_lowercase #TODO - delete
+                result_str = ''.join(random.choice(letters) for i in range(5)) #TODO - delete
+                ClientSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TODO - delete
                 ClientSock.connect(('localhost', tcp_port))
-                sentence = "Raz\n"
-                ClientSock.send(sentence.encode())
+                team_name = result_str+"\n" #TODO - change for team name
+                ClientSock.send(team_name.encode())
             except:
                 break
             try:
                 game_begins_message = ClientSock.recv(bufferSize).decode()
                 if game_begins_message != "":
                     print(game_begins_message)
-                while 1:
-                    char = msvcrt.getch()
-                    pressedKey = char.decode('ASCII')
-                    ClientSock.send(pressedKey.encode())
+                    t_end3 = time.time() + 10
+                    while time.time() < t_end3:
+                        char = msvcrt.getch()
+                        pressedKey = char.decode('ASCII')
+                        ClientSock.send(pressedKey.encode())
+                    print("Server disconnected, listening for offer requests...")
             except:
                 break
     except:
         break
+while True:
+    msg1 = UDPClientSocket.recvfrom(bufferSize)
+
--- hackaton/server.py
+++ hackaton/server.py
@@ -1,19 +1,28 @@
 import socket
+import _thread
 import time
 import struct
 import threading
+import random
 
 FORMAT = "32s 1s 40s 1s 256s 256s"
 bufferSize = 1024
 clients = []
 points = 0
+teams_list = []
+array = [1, 1, 2, 2]
+random.shuffle(array)
+counter1 = 0
+counter2 = 0
+clients = []
+team_1 = []
+team_2 = []
+index_counter = 0
+t_end = time.time() + 10
 
 
-ori = 5
-
-
-def play_the_game_thread(client, address):
-    global points
+def play_the_game_thread_group_1(client, address):
+    global counter1
     lock = threading.RLock()
     t_end3 = time.time() + 10
     while time.time() < t_end3:
@@ -21,32 +30,84 @@ def play_the_game_thread(client, address):
             key = client.recv(bufferSize)
             keyboard_press = key.decode()
             if keyboard_press != "":
-                print(keyboard_press)
                 with lock:
-                    points += 1
+                    counter1 = counter1 + 1
         except:
             continue
-        print(points)
+    return
+
+
+def play_the_game_thread_group_2(client, address):
+    global counter2
+    lock = threading.RLock()
+    t_end3 = time.time() + 10
+    while time.time() < t_end3:
+        try:
+            key = client.recv(bufferSize)
+            keyboard_press = key.decode()
+            if keyboard_press != "":
+                with lock:
+                    counter2 = counter2 + 1
+        except:
+            continue
+    return
+
+
+def on_new_client(clientSocket, addr):
+    global t_end
+    global clients
+    global team_1
+    global team_2
+    global index_counter
+    global counter1
+    global counter2
+    lock = threading.RLock()
+    lock1 = threading.RLock()
+    lock2 = threading.RLock()
+    try:
+        group_name = clientSocket.recv(1024).decode()
+        if group_name != "":
+            with lock:
+                clients.append(group_name)
+            while len(clients) < 4:
+                time.sleep(1)
+            index_counter = len(team_1) + len(team_2)
+            if index_counter < 4:
+                group_num = array[index_counter]
+                if group_num == 1:
+                    with lock1:
+                        team_1.append(group_name)
+                if group_num == 2:
+                    with lock2:
+                        team_2.append(group_name)
+            while len(team_1) + len(team_2) < 4:
+                time.sleep(1)
+        while time.time() < t_end:
+            time.sleep(1)
+        welcome_game = "Welcome to Keyboard Spamming Battle Royale.\nGroup 1:\n==\n" + team_1[0] + team_1[1] \
+                       + "Group 2:\n==\n" + team_2[0] + team_2[1] + "\nStart pressing keys on your keyboard as fast as " \
+                                                                    "you can!!\n "
+        clientSocket.send(welcome_game.encode())
+        if group_num == 1:
+            _thread.start_new_thread(play_the_game_thread_group_1, (clientSocket, addr))
+        if group_num == 2:
+            _thread.start_new_thread(play_the_game_thread_group_2, (clientSocket, addr))
+        time.sleep(10)
+        return
+    except:
+        print("error occurred")
 
 
 def group_name_client_thread():
     try:
+        global t_end
         TCPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
         TCPServerSocket.bind(("", 10))
-        TCPServerSocket.listen(4)
-        t_end2 = time.time() + 10
-        while time.time() < t_end2:
+        TCPServerSocket.listen(1)
+        while time.time() < t_end:
             clientSocket, addr = TCPServerSocket.accept()
-            clients.append((clientSocket, addr))
-            sentence = clientSocket.recv(1024).decode()
-            if sentence != "":
-                print(sentence)
-            break
-        for (client, addr) in clients:
-            sentence = "Game Begins\n"
-            client.send(sentence.encode())
-            thread = threading.Thread(target=play_the_game_thread, args=(client, addr))
-            thread.start()
+            group_thread = threading.Thread(target=on_new_client, args=(clientSocket, addr))
+            group_thread.start()
         return
     except:
         print("Wrong type of message received")
@@ -61,18 +122,31 @@ localPort = 7700
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 UDPServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 UDPServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-UDPServerSocket.bind(('', localPort)) # TODO: write the IP
-
-print("Server started' listening on IP address 172.1.0.88")
-thread1 = threading.Thread(target=group_name_client_thread, args=())
-thread1.start()
-t_end = time.time() + 10
+UDPServerSocket.bind(('', localPort))  # TODO - write the IP
+print("Server started' listening on IP address 172.1.0.52")
+thread = threading.Thread(target=group_name_client_thread, args=())
+thread.start()
 while time.time() < t_end:
     try:
         MSG = struct.pack('<3Q', 0xfeedbeef, 0x2, 0xA)
         UDPServerSocket.sendto(MSG, ('<broadcast>', 13117))
-        print("message sent!")
         time.sleep(1)
-        print(points)
     except:
         time.sleep(1)
+# calculate
+time.sleep(10)
+print("try to calulate results")
+if counter2 > counter1:
+    winning_group = "Group 1"
+    team_winners = team_1[0] + team_1[1]
+else:
+    winning_group = "Group 2"
+    team_winners = team_2[0] + team_2[1]
+finish_message = "Game over!\nGroup 1 typed in " + str(counter1) + " characters. Group 2 typed in " \
+                 + str(counter2) + " characters.\n" + winning_group + " wins!\n\n" \
+                 + "Congratulations to the winners:\n==\n" + "" + team_winners
+print(finish_message)
+while True:
+    MSG = struct.pack('<3Q', 0xfeedbeef, 0x2, 0xA)
+    UDPServerSocket.sendto(MSG, ('<broadcast>', 13117))
+    time.sleep(1)
